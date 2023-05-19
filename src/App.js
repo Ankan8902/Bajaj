88f2bde27918d9ecc2e6a640b21f83e12bdcf801
Ankan <ankankundu2002@yahoo.com>
2023-05-19 19:30:26 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    axios
+      .get(
+        "https://raw.githubusercontent.com/dixitsoham7/dixitsoham7.github.io/main/index.json"
+      )
+      .then((res) => setData(res.data.employees));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="filter">
+      <table className="App">
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Designation</th>
+          <th>Skills</th>
+          <th>Projects</th>
+        </tr>
+        {data &&
+          data.map((emp) => (
+            <tr>
+              <td>{emp.id}</td>
+              <td>{emp.name}</td>
+              <td>{emp.designation}</td>
+              <td>
+                {emp.skills.length > 0
+                  ? emp.skills.map((p) => <p>{p}, </p>)
+                  : "no skills"}
+              </td>
+              <td>
+                {emp.projects
+                  ? emp.projects.map((p) => <p>{p.name},</p>)
+                  : "no projects"}
+              </td>
+            </tr>
+          ))}
+      </table>
     </div>
   );
 }
